72cf9cf62c3be74fcba8e7d864c610bfcf351922
sitarcik <sitarcik@topas.dnp.fmph.uniba.sk>
2022-12-06 10:50:41 +0100

    remove clouds and visibility - caused some problems in output csvs

--- web_scraping.py
+++ web_scraping.py
@@ -20,12 +20,13 @@ datetime = dparser.parse(datetime_string, fuzzy=True)
 for station in stations.keys():
     station_info[station] = soup.find('td', string=stations[station]).find_parent('tr').find_all('td') # find all td elements which are children of tr element
     df = pd.DataFrame({
-                        'datetime':datetime,
+                        'datetime':[datetime],
                         'temperature':float(station_info[station][1].get_text().split(' ')[0]), # extract float from the string
                         'pressure':float(station_info[station][5].get_text().split(' ')[0]), # extract float from the string
-                        'clouds':[station_info[station][6].get_text()],
-                        'visibility':[station_info[station][7].get_text()]
+                       # 'clouds':[station_info[station][6].get_text()],
+                       # 'visibility':[station_info[station][7].get_text()]
                     })
+    print(df)
     if platform.system() == 'Linux':
         df.to_csv(current_path+'/data_'+station+'.csv', mode='a', index=False, header=False)
     elif platform.system() == 'Windows':
