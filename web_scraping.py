d27198e78b100bea0e71c3a95d82d0f3fda0e976
Adam Sitarcik <adamsitarcik21@gmail.com>
2022-12-22 12:55:42 +0100

    upload data and update scrapping script

--- web_scraping.py
+++ web_scraping.py
@@ -4,6 +4,7 @@ import dateutil.parser as dparser
 import pandas as pd
 import os
 import platform
+import numpy as np
 
 current_path = os.path.split(os.path.realpath(__file__))[0]
 
@@ -19,10 +20,17 @@ datetime = dparser.parse(datetime_string, fuzzy=True)
 
 for station in stations.keys():
     station_info[station] = soup.find('td', string=stations[station]).find_parent('tr').find_all('td') # find all td elements which are children of tr element
+    temperature = station_info[station][1].get_text()
+    pressure = station_info[station][5].get_text()
+    if len(temperature) !=0:
+        temperature = float(temperature.split(' ')[0])
+    if len(pressure) > 1:
+        pressure = float(pressure.split(' ')[0])
+
     df = pd.DataFrame({
                         'datetime':[datetime],
-                        'temperature':float(station_info[station][1].get_text().split(' ')[0]), # extract float from the string
-                        'pressure':float(station_info[station][5].get_text().split(' ')[0]), # extract float from the string
+                        'temperature':temperature, # extract float from the string
+                        'pressure':pressure, # extract float from the string
                        # 'clouds':[station_info[station][6].get_text()],
                        # 'visibility':[station_info[station][7].get_text()]
                     })
