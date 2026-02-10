13fd5fdcf2db94061295309f984ba48121e9af52
Adam Sitarcik <adamsitarcik21@gmail.com>
2026-02-10 16:32:36 +0100

    upload data at $(date)

--- upload_data.sh
+++ upload_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/usr/bin:/bin
 /usr/bin/git add .
-/usr/bin/git commit -m 'upload data' 
-/usr/bin/git push
+/usr/bin/git commit -m 'upload data at $(date)' 
+#/usr/bin/git push
 
